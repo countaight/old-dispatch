4a49ac3abb13a83e6b76ea603ccf8d9b96cf13a9
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-10-20 14:52:21 -0500

    Load and authorize resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   include SessionsHelper
+
+  rescue_from CanCan::AccessDenied do |exception|
+  	if current_user
+  		redirect_to profile_path
+  	else
+  		redirect_to login_path
+  	end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	skip_before_filter :verify_authenticity_token
+	load_and_authorize_resource
 
 	def index
 		@users = User.all
