283a3bdd480daa7f86ce2be6cd05d794a5166022
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2016-10-24 14:03:25 -0500

    Make websocket property of class

--- client/app/bundles/Map/components/Map.jsx
+++ client/app/bundles/Map/components/Map.jsx
@@ -8,6 +8,8 @@ import DriverList from './DriverList.jsx';
 
 
 export default class Map extends React.Component {
+	ws: null;
+
 	constructor (props) {
 		super(props);
 		this.currentUserCoords = JSON.parse(this.props.currentUser.coordinates)
@@ -18,7 +20,6 @@ export default class Map extends React.Component {
 									],
 			zoom: 9,
 			selectedKey: null,
-			ws: {},
 			loadedUsers: this.props.users,
 		}
 	}
@@ -28,7 +29,7 @@ export default class Map extends React.Component {
 		var uri = "wss://" + window.document.location.host + "/mapsocket";
 		var ws = new WebSocket(uri);
 
-		this.setState({ ...this.state, ws });
+		this.ws = ws
 
 		ws.onopen = (e) => {
 			console.log('Connected');
@@ -42,8 +43,8 @@ export default class Map extends React.Component {
 	}
 
 	componentWillUnmount () {
-		this.state.ws.close();
-		this.setState({ ...this.state, ws: {} })
+		this.ws.close();
+		this.ws = null;
 	}
 
 	_updateUser (loadUser) {
