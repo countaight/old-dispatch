1af86fd8af6eee2986c8d95af2873ee26be902fb
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-01-09 14:04:38 -0600

    Add pick up / delivery column to place assigments.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230163226) do
+ActiveRecord::Schema.define(version: 20170109194432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161230163226) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "pu_del"
   end
 
   create_table "places", force: :cascade do |t|
