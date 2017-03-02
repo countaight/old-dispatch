b54e872dfbebc98775b530b43dbc12c326f4f17f
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-03-02 14:14:45 -0600

    Move place assignments to admin namespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
 	validates_presence_of :password, on: :create
 	serialize :coordinates
 
-	has_many :place_assignments
+	has_many :place_assignments, class_name: 'Admin::PlaceAssignment'
 	has_many :places, through: :place_assignments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :admin do 
     resources :users, only: [:index, :destroy]
     resources :places
+    resources :place_assignments
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
