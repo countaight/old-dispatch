4ae29e2fbb1ded0b16465cf446ab344346fe0704
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-03-16 14:54:56 -0500

    Make Place background dynamically change color based on distance from the driver

--- client/app/bundles/Map/components/Place.jsx
+++ client/app/bundles/Map/components/Place.jsx
@@ -16,6 +16,24 @@ export default class Place extends React.Component {
 		this.props.deletePlace(this.props.place.assignment.id);
 	}
 
+	getStyle() {
+		const { place } = this.props;
+
+		let style = {
+			height: '100%',
+			top: 0,
+			right: 0,
+		};
+
+		style.position = 'absolute';
+
+		style.backgroundColor = place.assignment.pu_del == 'DEL' ? '#efc576' : '#00b99d';
+
+		style.width = this.determineDistance() > 5 ? '100%' : this.determineDistance()/5 * 100 + "%"
+
+		return style
+	}
+
 	determineDistance() {
 		const { userLocation, place } = this.props;
 		return distance(userLocation.lat, userLocation.lng, place.place.location.lat, place.place.location.lng);
@@ -27,6 +45,7 @@ export default class Place extends React.Component {
 			<li
 				className={"place " + place.assignment.pu_del}
 			>
+				<div className="progress-bar" style={this.getStyle()}/>
 				<input onChange={this._handleCheckbox.bind(this)} type="checkbox" checked={place.assignment.delivered} />
 				<span className="place-name">{place.place.name + " "}{this.determineDistance().toFixed(1)}</span>
 				{place.assignment.delivered ? <span className="delete-button" style={{cursor: 'pointer'}} onClick={this._handleDelete.bind(this)}>Delete</span> : <span />}
