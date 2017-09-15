ed9d1d3378e58d4918a14e9b1702ab3615028137
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-09-15 11:27:38 -0500

    Remove distance method

--- app/models/admin/place_assignment.rb
+++ app/models/admin/place_assignment.rb
@@ -1,9 +1,4 @@
 class Admin::PlaceAssignment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place, class_name: 'Admin::Place'
-	attr_reader :distance
-
-	def distance
-		self.place.location
-	end
 end
