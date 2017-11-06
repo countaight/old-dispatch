b5d5ac53ff36b76d4009c3cafb3b054e646c9651
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2017-11-06 15:29:16 -0600

    Upgrade prop-types, remove css transition group

--- app/javascript/mapFeature/components/Driver.jsx
+++ app/javascript/mapFeature/components/Driver.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import DriverInfo from './DriverInfo.jsx';
 import SearchInput from './SearchInput.jsx';
--- app/javascript/mapFeature/components/MapMarker.jsx
+++ app/javascript/mapFeature/components/MapMarker.jsx
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Motion, presets, spring } from 'react-motion';
 import Moment from 'moment';
 
 const K_SIZE = 18;
@@ -18,16 +19,26 @@ export default class MapMarker extends React.Component {
   }
 
   render () {
-    const style = this.props.$hover ? hoverStyle : greatPlaceStyle
+    const style = this.props.$hover ? hoverStyle : greatPlaceStyle;
+
     return (
       <div>
-        <ReactCSSTransitionGroup
-          transitionName={"bubble"}
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={300}
+        <Motion
+          defaultStyle={{ width: 0, height: 0, top: 0 }}
+          style={{
+            width: spring(this.props.$hover || this.props.id == this.props.selectedKey ? 150 : 0, presets.wobbly),
+            height: spring(this.props.$hover || this.props.id == this.props.selectedKey ? 70 : 0, presets.wobbly),
+            top: spring(this.props.$hover || this.props.id == this.props.selectedKey ? -65 : 0, presets.wobbly)
+          }}
         >
-          {this.props.$hover || this.props.id == this.props.selectedKey ? <div className={"bubble"}><h1 className={"bubble-title"}>{this.props.title}</h1><p className={"bubble-text"}>Last Updated: {Moment(this.props.lastUpdated).calendar()}</p></div> : ""}
-        </ReactCSSTransitionGroup>
+          {value => {
+            return <div className={'bubble'} style={{width: value.width, height: value.height}}>
+              <h1 className={"bubble-title"}>{this.props.title}</h1>
+              <p className={"bubble-text"}>Last Updated: {Moment(this.props.lastUpdated).calendar()}</p>
+            </div>
+            }
+          }
+        </Motion>
         <div style={style}>
           <div style={innerStyle} />
         </div>
